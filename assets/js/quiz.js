2aef8a89d8f7484dfb535fa8a2da653614002d76
Stephanie Jauch <sjauch21@gmail.com>
2023-06-15 16:21:24 -0500

    js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,21 +1,16 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
-	function showQuestions(questions, quizContainer){
-		// code will go here
-	}
-
-	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
-	}
-
-	// show the questions
-	showQuestions(questions, quizContainer);
-
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
-}
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
 
 var myQuestions = [
 	{
@@ -24,7 +19,6 @@ var myQuestions = [
 			a: 'Curly Braces',
 			b: 'Parenthese',
 			c: 'Quotations',
-            d: 'Ampersands'
 		},
 		correctAnswer: 'c'
 	},
@@ -33,19 +27,170 @@ var myQuestions = [
 		answers: {
 			a: 'true/false',
 			b: 'a number',
-			c: 'an alien species'
-            d: 'an array'
+			c: 'an alien species',
 		},
 		correctAnswer: 'a'
-	}
+	},
     {
 		question: "To terminate a line of code in JavaSript, you must use:",
 		answers: {
 			a: '?',
-			b: '#',
-			c: '*'
-            d: ';'
+			b: ';',
+			c: '*',
 		},
-		correctAnswer: 'd'
-	}
+		correctAnswer: 'b'
+	},
 ];
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		var output = [];
+	var answers;
+
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+		
+		// first reset the list of answers
+		answers = [];
+
+		// for each available answer to this question...
+		for(letter in questions[i].answers){
+
+			// ...add an html radio button
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+		// add this question and its answers to the output
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	// finally combine our output list into one string of html and put it on the page
+	quizContainer.innerHTML = output.join('');
+}
+
+showQuestions(questions, quizContainer);
+
+	}
+
+	function showResults(questions, quizContainer, resultsContainer){
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+	
+	// keep track of user's answers
+	var userAnswer = '';
+	var numCorrect = 0;
+	
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+
+		// find selected answer
+		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+		
+		// if answer is correct
+		if(userAnswer===questions[i].correctAnswer){
+			// add to the number of correct answers
+			numCorrect++;
+			
+			// color the answers green
+			answerContainers[i].style.color = 'lightgreen';
+		}
+		// if answer is wrong or blank
+		else{
+			// color the answers red
+			answerContainers[i].style.color = 'red';
+		}
+	}
+
+	// show number of correct answers out of total
+	resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
+
+
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+
+function init() {
+    getWins();
+    getlosses();
+  }
+
+  function startGame() {
+    isWin = false;
+    timerCount = 10;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    renderBlanks()
+    startTimer()
+  }
+
+  function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 1000);
+  }
+  
+  function setWins() {
+    win.textContent = winCounter;
+    localStorage.setItem("winCount", winCounter);
+  }
+  
+  // Updates lose count on screen and sets lose count to client storage
+  function setLosses() {
+    lose.textContent = loseCounter;
+    localStorage.setItem("loseCount", loseCounter);
+  }
+  
+  // These functions are used by init
+  function getWins() {
+    // Get stored value from client storage, if it exists
+    var storedWins = localStorage.getItem("winCount");
+    // If stored value doesn't exist, set counter to 0
+    if (storedWins === null) {
+      winCounter = 0;
+    } else {
+      // If a value is retrieved from client storage set the winCounter to that value
+      winCounter = storedWins;
+    }
+    //Render win count to page
+    win.textContent = winCounter;
+  }
+  
+  function getlosses() {
+    var storedLosses = localStorage.getItem("loseCount");
+    if (storedLosses === null) {
+      loseCounter = 0;
+    } else {
+      loseCounter = storedLosses;
+    }
+    lose.textContent = loseCounter;
+  }
+
+  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
