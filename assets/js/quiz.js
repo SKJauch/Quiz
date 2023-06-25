b913a4176ef0493a4f9051f9d13937e81063f1d6
Stephanie Jauch <sjauch21@gmail.com>
2023-06-24 22:05:21 -0500

    render initials

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,18 @@
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
-var submitBtn = document.querySelector("#submit-btn");
-var initialForm = document.querySelector("#initial-form");
-var initialsList = document.querySelector("#initials-list");
+// var saveBtn = document.querySelector("#save-btn");
 var initialsInput = document.querySelector("#initials-text");
+var initialsForm = document.querySelector("#score-form");
+var initialsList = document.querySelector("#initials-list");
+var initialsSpan = document.querySelector("#initials");
+// var cardDiv = document.querySelector("#card");
+
+// var initialForm = document.querySelector("#initial-form");
+// var initialsList = document.querySelector("#initials-list");
+// var initialsInput = document.querySelector("#initials-text");
 
 var winCounter = 0;
 var loseCounter = 0;
@@ -49,72 +56,60 @@ function init() {
 
 var currentQuestionIndex = 0;
 
+function resetFeedback() {
+  feedbackElement.textContent = "";
+}
+
 function startGame() {
-  currentQuestionIndex = 0;
-  timerCount = 10;
+  isWin = false;
+  timerCount = 30;
   startButton.disabled = true;
+  startTimer();
+  setNextQuestion();
 }
 
-function renderQuestion(questionIndex) {
-  var questionData = quizData[questionIndex];
-  var questionContainer = document.querySelector("#question-container");
-  questionContainer.innerHTML = ""; // Clear previous question
-
-  // Create question element
-  var questionElement = document.createElement("h2");
-  questionElement.textContent = questionData.question;
-  questionContainer.appendChild(questionElement);
-
-  // Create options elements
-  var optionsContainer = document.createElement("div");
-  optionsContainer.className = "options-container";
-
-  for (var option in questionData.options) {
-    var optionElement = document.createElement("label");
-    var radioInput = document.createElement("input");
-    radioInput.type = "radio";
-    radioInput.name = "question" + questionIndex;
-    radioInput.value = option;
-    optionElement.appendChild(radioInput);
-    optionElement.appendChild(
-      document.createTextNode(questionData.options[option])
-    );
-    optionsContainer.appendChild(optionElement);
-  }
+function setNextQuestion() {
+  resetFeedback();
+  showQuestion(quizData[currentQuestionIndex]);
 }
 
-function checkQuiz() {
-  var selectedOption = document.querySelector(
-    'input[name="question' + currentQuestionIndex + '"]:checked'
-  );
-  if (selectedOption) {
-    var selectedAnswer = selectedOption.value;
-    if (selectedAnswer === quizData[currentQuestionIndex].correctAnswer) {
-      // Correct answer
-      console.log("Question " + (currentQuestionIndex + 1) + " - Correct!");
-    } else {
-      // Incorrect answer
-      console.log("Question " + (currentQuestionIndex + 1) + " - Incorrect!");
-    }
-    currentQuestionIndex++; // Move to the next question
+// Function to display a question
+function showQuestion(question) {
+  questionElement.textContent = question.question;
+  choicesElement.innerHTML = "";
 
-    if (currentQuestionIndex < quizData.length) {
-      renderQuestion(currentQuestionIndex); // Render the next question
-    } else {
-      endQuiz(); // No more questions, end the quiz
-    }
+  question.choices.forEach((choice) => {
+    const choiceElement = document.createElement("li");
+    choiceElement.textContent = choice;
+    choiceElement.addEventListener("click", handleAnswerClick);
+    choicesElement.appendChild(choiceElement);
+  });
+}
+
+function handleAnswerClick(event) {
+  const selectedChoice = event.target;
+  const selectedAnswer = selectedChoice.textContent;
+  const currentQuestion = questions[currentQuestionIndex];
+
+  if (selectedAnswer === currentQuestion.answer) {
+    // Correct answer
+    feedbackElement.textContent = "Correct!";
   } else {
-    // No answer selected
-    console.log(
-      "Question " + (currentQuestionIndex + 1) + " - No answer selected!"
-    );
+    // Incorrect answer
+    feedbackElement.textContent = "Wrong!";
+    timeLeft -= 10; // Subtract 10 seconds for an incorrect answer
   }
-}
 
-function endQuiz() {
-  clearInterval(timer);
-  // Display final results or perform any other actions
-  startButton.disabled = false;
+  // Move to the next question
+  currentQuestionIndex++;
+
+  // Check if there are more questions
+  if (currentQuestionIndex < questions.length) {
+    setNextQuestion();
+  } else {
+    // No more questions, end the quiz
+    endQuiz();
+  }
 }
 
 function startTimer() {
@@ -123,6 +118,43 @@ function startTimer() {
     timerElement.textContent = timerCount;
     if (timerCount === 0) {
       clearInterval(timer);
+    }
+  }, 1000);
+}
+
+function winGame() {
+  wordBlank.textContent = "YOU WON!!!🏆 ";
+  winCounter++;
+  startButton.disabled = false;
+  setWins();
+}
+
+// The loseGame function is called when timer reaches 0
+function loseGame() {
+  loseCounter++;
+  startButton.disabled = false;
+  setLosses();
+}
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
     }
   }, 1000);
 }
@@ -142,7 +174,7 @@ function getWins() {
   if (storedWins === null) {
     winCounter = 0;
   } else {
-    winCounter = parseInt(storedWins); // Parse the stored value as an integer
+    winCounter = parseInt(storedWins);
   }
   win.textContent = winCounter;
 }
@@ -157,96 +189,140 @@ function getlosses() {
   lose.textContent = loseCounter;
 }
 
+startButton.addEventListener("click", startGame);
+
+// Calls init() so that it fires when page opened
+init();
+
+// Bonus: Add reset button
+var resetButton = document.querySelector(".reset-button");
+
 function resetGame() {
+  // Resets win and loss counts
   winCounter = 0;
   loseCounter = 0;
+  // Renders win and loss counts and sets them into client storage
   setWins();
   setLosses();
 }
 
-startButton.addEventListener("click", startGame);
 
-var initials = [];
+var initialsInput = document.querySelector("#initials-text");
+var initialsList = document.querySelector("#initials-list");
 
-// The following function renders items in a todo list as <li> elements
+// Render initials to the page
 function renderInitials() {
-  // Clear todoList element and update todoCountSpan
+  // Clear the initialsList element
   initialsList.innerHTML = "";
-  initialsCountSpan.textContent = initials.length;
 
-  // Render a new li for each todo
+  // Render a new li for each initial
   for (var i = 0; i < initials.length; i++) {
     var initial = initials[i];
 
     var li = document.createElement("li");
     li.textContent = initial;
-    li.setAttribute("data-index", i);
 
-    var button = document.createElement("initials-button");
-
-    li.appendChild(button);
     initialsList.appendChild(li);
   }
 }
-function init() {
-  // Get stored todos from localStorage
-  var storedInitials = JSON.parse(localStorage.getItem("initials"));
-
-  // If todos were retrieved from localStorage, update the todos array to it
-  if (storedInitials !== null) {
-    initials = storedInitials;
-  }
-
-  // This is a helper function that will render todos to the DOM
-  renderInitials();
-}
-
-function storeInitials() {
-  // Stringify and set key in localStorage to todos array
-  localStorage.setItem("initials", JSON.stringify(initials));
-}
 
 // Add submit event to form
-initialForm.addEventListener("submit", function (event) {
+initialsInput.addEventListener("keyup", function(event) {
   event.preventDefault();
 
-  var initialText = initialInput.value.trim();
+  // Check if Enter key is pressed
+  if (event.keyCode === 13) {
+    var enteredInitials = initialsInput.value.trim();
 
-  // Return from function early if submitted todoText is blank
-  if (initialText === "") {
-    return;
+    // Return from function early if enteredInitials is blank
+    if (enteredInitials === "") {
+      return;
+    }
+
+    // Add new enteredInitials to initials array, clear the input
+    initials.push(enteredInitials);
+    initialsInput.value = "";
+
+    // Render updated initials to the page
+    renderInitials();
   }
-
-  // Add new todoText to todos array, clear the input
-  initials.push(initialsText);
-  initialInput.value = "";
-
-  // Store updated todos in localStorage, re-render the list
-  storeInitials();
-  renderInitials();
 });
 
-// Add click event to todoList element
+// var initials = [];
+
+// // The following function renders items in a todo list as <li> elements
+// function renderInitials() {
+//   // Clear todoList element and update todoCountSpan
+//   initialsList.innerHTML = "";
+//   initialsSpan.textContent = initials.length;
+
+//   // Render a new li for each todo
+//   for (var i = 0; i < initials.length; i++) {
+//     var initials = initials[i];
+
+//     var li = document.createElement("li");
+//     li.textContent = initials;
+//     li.setAttribute("data-index", i);
+
+//     li.appendChild(button);
+//     initialsList.appendChild(li);
+//   }
+// }
+
+
+// function init() {
+//   var storedInitials = JSON.parse(localStorage.getItem("initials"));
+
+//   if (storedInitials !== null) {
+//     initials = storedInitials;
+//   }
+
+//   // This is a helper function that will render todos to the DOM
+//   renderInitials();
+// }
+
+// function storeInitials() {
+//   // Stringify and set key in localStorage to todos array
+//   localStorage.setItem("initials", JSON.stringify(initials));
+// }
+
+// // Add submit event to form
+// initialsForm.addEventListener("submit", function(event) {
+//   event.preventDefault();
+
+//   var initialsText = initialsInput.value.trim();
+
+//   // Return from function early if submitted todoText is blank
+//   if (initialsText === "") {
+//     return;
+//   }
+
+//   // Add new todoText to todos array, clear the input
+//   initials.push(initialsText);
+//   initialsInput.value = "";
+
+//   // Store updated todos in localStorage, re-render the list
+//   storeInitials();
+//   renderInitials();
+// });
+
+// // Add click event to todoList element
 // initialsList.addEventListener("click", function(event) {
 //   var element = event.target;
 
-  // Checks if element is a button
+//   // Checks if element is a button
 //   if (element.matches("button") === true) {
 //     // Get its data-index value and remove the todo element from the list
 //     var index = element.parentElement.getAttribute("data-index");
 //     initials.splice(index, 1);
 
-    // Store updated todos in localStorage, re-render the list
+//     // Store updated todos in localStorage, re-render the list
 //     storeInitials();
 //     renderInitials();
 //   }
 // });
 
+// // Calls init to retrieve data and render it to the page on load
+// init()
 
-
-// Calls init() so that it fires when the page is opened
-init();
-
-// // Bonus: Add reset button
-// var resetButton = document.querySelector(".reset-button");
-// resetButton.addEventListener("click", resetGame);
+resetButton.addEventListener("click", resetGame);
